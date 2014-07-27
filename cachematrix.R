f8bf1ce54f4b20b0fab1c18f24cbcd6e744b4d07
Ed <ed@ed-ubuntu.(none)>
2014-07-27 14:46:12 -0400

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,25 +4,26 @@
 ## that environment
 #  0: 20140725 - initial version from the assignment
 #  1: 20140725 - modified makeCacheMatrix() to show structure then added trace prints
+#  2: 20140727 - more comments and minor fixes, cmt out messages
 #
 makeCacheMatrix <- function(x = matrix()) { 
     inv <- NULL                                                     # orginal       
-    set <- function(y) {                                            # orginal
-        x <<- y                                                     # orginal
-        inv <<- NULL                                                # orginal
-        message("set", x, inv)                                      # added for tracing
+    set <- function(y) {                                            # orginal - creates set function
+        x <<- y                                                     # orginal - stores matrix in parent env
+        inv <<- NULL                                                # orginal - creates null inv in parent env
+#        message("set", x, inv)                                      # added for tracing
     }                                                               # orginal
     get <- function() {                                             # modified to show structure
-        x                                                           # orginal
-        message("get", x)                                           # added for tracing
+        x                                                           # orginal - gets matrix from parent env
+#        message("get", x)                                           # added for tracing
     }                                                               # modified to show structure
     setinv <- function(z) {                                         # modified to show structure
-        inv <<- z                                                   # orginal
-        message("setinv", x, inv)                                   # added for tracing     
+        inv <<- z                                                   # orginal - stores inv in parent env
+#        message("setinv", z, inv)                                   # added for tracing     
     }                                                               # modified to show structure
     getinv <- function() {                                          # modified to show structure
-        m                                                           # orginal
-        message("getinv", m)                                        # added for tracing     
+        inv                                                         # orginal - gets inv from parent env
+#        message("getinv", inv)                                      # added for tracing     
     }                                                               # modified to show structure
     list(set = set, get = get,setmean = setmean,getmean = getmean)  # orginal
 }
@@ -33,19 +34,18 @@ makeCacheMatrix <- function(x = matrix()) {
 #  Dependencies: must call makeCacheMatrix() first
 #
 #  0: 20140725 - initial version from the assignment, approach based on the vector mean example
-#  1: 20140725 - 
+#  1: 20140725 - cmt out message
 #
 cacheSolve <- function(x, ...) { 
     inv <- x$getinv()                                               # get the inverse
     if(!is.null(inv)) {                                             # if there is a cached version
-        message("getinv", inv)                                      #   trace
+#        message("getinv", inv)                                      #   trace
         return(inv)                                                 #   return it
     } else {                                                        # else 
         data <- x$get()                                             #   get the matrix
         inv <- solve(data, ...)                                     #   compute the invers
         x$setinv(inv)                                               #   cache it
-        message("compute & cache inv",inv)                          #   trace it
+#        message("compute & cache inv",inv)                          #   trace it
         return(inv)                                                 #   return it
     }
-    message("can't get here!!!")                                    # trace - flow error if get here
 }                                                                   # should never return fom here
